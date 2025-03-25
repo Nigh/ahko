ff231b66a482999a3a77d66600f79a659027faad
xianii <xianyi.xia@outlook.com>
2025-03-25 18:02:46 +0800

    really fix updater issue

--- updater.c
+++ updater.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <process.h>
+#include <windows.h>
 #include "updater.h"
+
+void newProcess(const char *path) {
+	STARTUPINFO si = { sizeof(si) };
+	PROCESS_INFORMATION pi;
+	if (CreateProcess(path, NULL, NULL, NULL, FALSE, 0, NULL, NULL, &si, &pi)) {
+		CloseHandle(pi.hProcess);
+		CloseHandle(pi.hThread);
+	} else {
+		printf("Failed to start process. Error: %d\n", GetLastError());
+	}
+}
 
 int main(void) {
 	system("powershell -command \"Expand-Archive -Force " downloadFilename " .\"");
 	remove(downloadFilename);
-	execv(binaryFilename, NULL);
+	newProcess(binaryFilename);
 	return 0;
 }
